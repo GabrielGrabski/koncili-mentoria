27795e6e16b19bcda786d812648c3b783088fc46
Gabriel Grabski <gabrielgrabski001@gmail.com>
2023-09-18 12:52:16 -0300

    001 - remove unused import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AccountComponent } from './features/account/account.component';
 
 const routes: Routes = [
   {
     path: 'features',
     loadChildren: () => import('./features/features.module').then(m => m.FeaturesModule)
-
   }
 ];
 
